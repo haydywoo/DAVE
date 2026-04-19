2f1e88a0346ddd8984462c67858d3842e2282565
Haydn Phillips <haydynphillips@gmail.com>
2026-04-19 19:55:13 +0100

    chore(tooling): disable React prop-types and unescaped-entities for TS/MDX code

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -47,5 +47,7 @@ module.exports = {
   rules: {
     'react/react-in-jsx-scope': 'off',
     'react/jsx-uses-react': 'off',
+    'react/prop-types': 'off',
+    'react/no-unescaped-entities': 'off',
   },
 };
